33f6a221bb7dafe4eef659f3dd5dad2bbd7494c0
akj1991 <akhilonattu@gmail.com>
2022-02-03 12:36:52 +0530

    urls created

--- playersapp/urls.py
+++ playersapp/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('1st/',views.index),
+    path('2st/',views.index2),
 ]
--- playersapp/views.py
+++ playersapp/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse('this is players list for next match')
+def index2(request):
+    return render(request,'ak.html')
 
